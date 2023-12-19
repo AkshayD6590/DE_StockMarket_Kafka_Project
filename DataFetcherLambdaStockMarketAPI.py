7cfa63302808b5a34b271ceb90627eef6692cf86
AkshayD6590 <105377500+AkshayD6590@users.noreply.github.com>
2023-12-19 22:29:44 +0530

    Update DataFetcherLambdaStockMarketAPI.py
    Working copy of lambda function on local machine

--- DataFetcherLambdaStockMarketAPI.py
+++ DataFetcherLambdaStockMarketAPI.py
@@ -1,64 +1,68 @@
-import http.client
+import requests
 import json
 from confluent_kafka import Producer
 
-def lambda_handler(event, context):
-    stock_data = fetch_stock_data()
-    print('The data is :', stock_data)
-    # Categorize and publish data to Kafka topics
-    for record in stock_data:
-        if record['identifier'] in ['AXISBANKEQN', 'HDFCBANKEQN']:
-            publish_to_kafka('Financial', record['stock_data'])
-        elif record['identifier'] in ['BPCLEQN', 'ONGCEQN']:
-            publish_to_kafka('OilGas', record['stock_data'])
-        elif record['identifier'] in ['ITCEQN', 'BRITANNIAEQN']:
-            publish_to_kafka('Healthcare', record['stock_data'])
-        elif record['identifier'] in ['ITCEQN', 'BRITANNIAEQN']:
-            publish_to_kafka('FMCG', record['stock_data'])
-        else:
-            return 'Other', record['identifier']    
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(stock_data)
-    }
-        
 def fetch_stock_data():
-    host = "latest-stock-price.p.rapidapi.com"
-    path = "/price"
-    query = "Indices=NIFTY%2050"
+    url = "https://latest-stock-price.p.rapidapi.com/price"
+
+    querystring = {"Indices": "NIFTY 50"}
 
     headers = {
-        'X-RapidAPI-Key': '6aaf24f081msh54447b87c027433p1eff79jsna78813754cb4',
-        'X-RapidAPI-Host': host
+        "X-RapidAPI-Key": "6aaf24f081msh54447b87c027433p1eff79jsna78813754cb4",
+        "X-RapidAPI-Host": "latest-stock-price.p.rapidapi.com"
     }
 
     try:
-        connection = http.client.HTTPSConnection(host)
-        connection.request("GET", f"{path}?{query}", headers=headers)
-        response = connection.getresponse()
+        response = requests.get(url, headers=headers, params=querystring)
 
-        if response.status == 200:
-            data = response.read()
-            return json.loads(data)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()  # Assuming the response is in JSON format
+            print("API request successful")
+
+            return data
         else:
-            return {
-                'error': f"Unexpected status code: {response.status}"
-            }
+            print(f"API request failed with status code {response.status_code}")
+            print("Response:", response.text),
+            return("Response:", response.text)
 
     except Exception as e:
-        return {
-            'error': f"Error fetching stock data: {e}"
-        }
-
-    finally:
-        connection.close()
+        print("An error occurred:", str(e))
 
 def publish_to_kafka(topic, message):
-    kafka_brokers = 'localhost:9092'
-    producer_config = {
-        'bootstrap.servers': kafka_brokers,
-    }
-    producer = Producer(producer_config)
-    producer.produce(topic, value=message)
-    producer.flush()
+    print('Inside Publish to kafka function', topic, message)  # for test
+    kafka_brokers = '127.0.0.1:9092'
+
+    producer_conf = {'bootstrap.servers': kafka_brokers}
+    
+    producer = Producer(producer_conf)
+
+    try:
+        # Produce a sample message to the specified topic
+        producer.produce(topic, value=message)
+        # Wait for any outstanding messages to be delivered and delivery reports received
+        producer.flush()
+        print(f"Sample message '{message}' sent to topic '{topic}' successfully.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+stock_data = fetch_stock_data()
+print('The data is :', stock_data)  #For Testing
+    
+for record in stock_data:
+
+    identifier = record['identifier']
+
+    if identifier == 'AXISBANKEQN' or identifier == 'HDFCBANKEQN':
+        publish_to_kafka('Financial', json.dumps(record))
+    elif identifier == 'BPCLEQN' or identifier == 'ONGCEQN':
+        publish_to_kafka('OilGas', json.dumps(record))
+    elif identifier == 'ITCEQN' or identifier == 'BRITANNIAEQN':
+        publish_to_kafka('Healthcare', json.dumps(record))
+    elif identifier == 'ITCEQN' or identifier == 'BRITANNIAEQN':
+        publish_to_kafka('FMCG', json.dumps(record))
+    else:
+        publish_to_kafka('Other',json.dumps(record))
+#test_var=lambda_handler()
+#print(test_var)    
